82bb458bd9ac39835bf0c4495a61e2de7ba1a601
031hoodboy <hoodboy@hoodboy.local>
2022-04-14 22:29:45 +0900

    feat: add modal

--- src/components/InputPost.js
+++ src/components/InputPost.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import axios from "axios";
 
 const InputPost = () => {
   const [content, setContent] = useState("");
   const [number, setNumber] = useState("");
+
+  const [open, setOpen] = useState(false);
+
+  const onOpen = () => {
+    setOpen(!open);
+  };
 
   const contentHandler = (e) => {
     e.preventDefault();
@@ -33,7 +39,7 @@ const InputPost = () => {
       .then((res) => console.log(res));
   };
 
-  const Alert = () => {
+  const Register = () => {
     alert("등록되었습니다.");
     setTimeout(function () {
       window.location.reload();
@@ -41,7 +47,7 @@ const InputPost = () => {
   };
 
   return (
-    <RegisterWrapper onSubmit={submitHandler}>
+    <RegisterWrapper>
       <RegisterInput
         placeholder="내용을 입력해주세요"
         value={content}
@@ -49,14 +55,48 @@ const InputPost = () => {
         type="text"
         maxLength="50"
       />
-      <RegisterButton type="submit" onClick={Alert}>
+      <RegisterButton onClick={onOpen} type="button">
         등록
       </RegisterButton>
+      {open ? (
+        <ModalContainer>
+          <Modal onSubmit={submitHandler}>
+            <ModalTitle>등록하시겠습니까?</ModalTitle>
+            <ModalContentWrapper>
+              <ModalContent>
+                전화번호를 입력하시면 추첨을 통해 커피 상품권을 드립니다.
+              </ModalContent>
+              <ModalPhoneInput
+                placeholder="전화번호"
+                value={number}
+                onChange={numberHandler}
+                type="text"
+                maxLength="13"
+              />
+            </ModalContentWrapper>
+            <RegisterButtonWrapper>
+              <CancelButton onClick={onOpen}>취소</CancelButton>
+              <ModalRegisterButton type="submit" onClick={Register}>
+                등록
+              </ModalRegisterButton>
+            </RegisterButtonWrapper>
+          </Modal>
+        </ModalContainer>
+      ) : null}
     </RegisterWrapper>
   );
 };
 
-const RegisterWrapper = styled.form`
+const bounce = keyframes`
+  0% {
+    transform: scale(0)
+  }
+  100% {
+    transform: scale(1)
+  }
+`;
+
+const RegisterWrapper = styled.div`
   width: 100%;
   display: flex;
   margin-top: 10px;
@@ -100,6 +140,85 @@ const RegisterButton = styled.button`
     padding: 8px 14px;
     font-size: 12px;
   }
+`;
+
+const ModalContainer = styled.div`
+  display: flex;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.3);
+`;
+
+const Modal = styled.form`
+  width: 450px;
+  background: #3e4044;
+  border-radius: 12px;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+  animation: ${bounce} 0.3s;
+`;
+
+const ModalTitle = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+const ModalContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+`;
+
+const ModalContent = styled.div``;
+
+const ModalPhoneInput = styled(RegisterInput)`
+  width: calc(100% - 48px);
+  margin-top: 25px;
+`;
+
+const RegisterButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+`;
+
+const CancelButton = styled.button`
+  min-width: 24px;
+  outline: none;
+  border: none;
+  box-shadow: none;
+  padding: 12px 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-weight: 700;
+  /* border: 0.5px solid #fff; */
+  background-color: #313235;
+  border-radius: 10px;
+  cursor: pointer;
+  min-width: 55px;
+  &:active {
+    transform: scale(0.97);
+  }
+  @media screen and (max-width: 876px) {
+    padding: 8px 14px;
+    font-size: 12px;
+  }
+`;
+
+const ModalRegisterButton = styled(CancelButton)`
+  background-color: #1859ff;
+  border: none;
+  margin-left: 10px;
 `;
 
 export default InputPost;
